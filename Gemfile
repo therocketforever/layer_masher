d2343ab60f909fb7ef1bf6b79a3fe44d22818db7
Don Wei <dwei@rbmtechnologies.com>
2015-12-28 17:09:11 -0500

    Initial configuration

--- Gemfile
+++ Gemfile
@@ -2,3 +2,12 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in layer_masher.gemspec
 gemspec
+
+group :pry do
+  gem "pry-doc"
+  gem "pry-docmore"
+  gem "pry-coolline"
+  gem "pry-awesome_print"
+  gem "pry-byebug"
+  gem "pry-stack_explorer"
+end
--- lib/layer_masher.rb
+++ lib/layer_masher.rb
@@ -1,5 +1,27 @@
-require "layer_masher/version"
+[ "layer_masher/version",
+  "thor",
+  "rainbow",
+  "rainbow/ext/string"
+].map {|gem| require gem }
 
 module LayerMasher
-  # Your code goes here...
+  class CLI < Thor
+    desc "smash", "Mash Layer Cake into FISH Environment Variables"
+    def smash
+      require"pry";binding.pry
+    end
+
+    desc "transmute", "Trasmute BASH `export`s to FISH `set -gx`"
+    def transmute
+    end
+
+    private
+
+    desc "load file", "Load Layer Cake file for transmutation"
+    def load_layer_cake
+      #layer_cake = []
+      #File.foreach "layer_cake.sh" {}
+    end
+
+  end
 end
